0f68a3edc27764ec19c33099154428e45bdca4db
youyoudezhuzhu <youyoudezhuzhu@users.noreply.github.com>
2026-08-15 13:09:04 +0800

    v2.1.0: AUSBC 音频引擎 + 设置弹窗中文化

--- monmon/build.gradle.kts
+++ monmon/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "com.youyoudezhuzhu.uvcmonitor"
     minSdk = 24
     targetSdk = 36
-    versionCode = 6
-    versionName = "2.0.1"
+    versionCode = 7
+    versionName = "2.1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
